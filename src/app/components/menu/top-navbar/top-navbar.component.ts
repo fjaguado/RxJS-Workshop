6be4b95f387e37a72bad933e23c4503bb7c0f3f2
jivanheerden <115975528+jivanheerden@users.noreply.github.com>
2023-03-06 10:15:59 +0100

    fixes

--- src/app/components/menu/top-navbar/top-navbar.component.ts
+++ src/app/components/menu/top-navbar/top-navbar.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { werfenLogo } from '../../../models/menu.model';
 
 @Component({
   selector: 'app-top-navbar',
   templateUrl: 'top-navbar.component.html',
   styleUrls: ['./top-navbar.component.scss'],
 })
-export class TopNavbarComponent {}
+export class TopNavbarComponent {
+  public werfenLogo = werfenLogo;
+}
