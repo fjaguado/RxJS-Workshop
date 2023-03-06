1af4ceca8522e61e7ec61bad7a3a6d6d22418634
jivanheerden <115975528+jivanheerden@users.noreply.github.com>
2023-03-06 12:38:22 +0100

    observable + observer

--- src/app/components/basic-definitions/basic-definitions-routing.module.ts
+++ src/app/components/basic-definitions/basic-definitions-routing.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ROUTES } from '../../models/menu.model';
 import { BasicDefinitionsComponent } from './basic-definitions.component';
-import { BASIC_INTRODUCTION } from './basic-definitions.data';
+import { BASIC_INTRODUCTION_SECTION, OBSERVABLE_SECTION, OBSERVER_SECTION } from './basic-definitions.data';
 
 const routes: Routes = [
   {
     path: ROUTES.INTRODUCTION,
     component: BasicDefinitionsComponent,
-    data: { selectedData: BASIC_INTRODUCTION },
+    data: { selectedData: BASIC_INTRODUCTION_SECTION },
+  },
+  {
+    path: ROUTES.OBSERVABLE,
+    component: BasicDefinitionsComponent,
+    data: { selectedData: OBSERVABLE_SECTION },
+  },
+  {
+    path: ROUTES.OBSERVER,
+    component: BasicDefinitionsComponent,
+    data: { selectedData: OBSERVER_SECTION },
   },
 ];
 
--- src/app/components/basic-definitions/basic-definitions.data.ts
+++ src/app/components/basic-definitions/basic-definitions.data.ts
@@ -1,6 +1,6 @@
 import { SECTION } from '../../models/data.model';
 
-export const BASIC_INTRODUCTION: SECTION[] = [
+export const BASIC_INTRODUCTION_SECTION: SECTION[] = [
   {
     title: 'What is RxJs?',
     body: `RxJS is a library for composing asynchronous and event-based programs by using observable sequences. It provides:
@@ -53,6 +53,121 @@ export const BASIC_INTRODUCTION: SECTION[] = [
         <b>Observers</b>: These are the consumers of the data streams emitted by observables.
       </li>
     </ol>
+    `,
+  },
+];
+
+export const OBSERVABLE_SECTION: SECTION[] = [
+  {
+    title: '',
+    body: `
+    <p>
+      An Observable represents a sequence of values which may be observed. Observables are attached to streams of data, and are responsible for processing that data and deliver it to Observers.
+    </p>
+    <p>
+      RxJS library implements the Observable primitive and adds operators that allow  composing sequences of asynchronous events together.
+    </p>
+    <p>
+      Observable is a very simple type for a unified API that can represent a wide variety of things: 
+      <ul>
+        <li>events</li>
+        <li>multiple values</li>
+        <li>single values</li>
+        <li>user interactions</li>
+        <li>streaming data</li>
+        <li>synchronous values</li>
+        <li>asynchronous values</li>
+      </ul>
+    </p>
+    `,
+  },
+  {
+    title: 'Observables are composable',
+    body: `
+    Observables can be composed with higher-order combinators (switchMap, concatMap, exhaustMap, etc.), so we're able to, for example, concatenate two streams of data.
+    `,
+  },
+  {
+    title: 'Observables are lazy',
+    body: `
+    Because they don't emit any data until the observable is subscribed. In contrast, a Promise executes a function passed to the constructor immediately without waiting for the call to then or other methods..
+    `,
+  },
+  {
+    title: 'Observables are cancellable',
+    body: `
+    Because once we call the unsubscribe method on an object returned by subscribe the observable will execute the cleanup function.
+    `,
+  },
+  {
+    title: 'What does an Observable guarantee?',
+    body: `
+    <ul>
+      <li>
+        Once it's complete, errored, or unsubscribed, you will get no more messages
+      </li>
+      <li>
+        Registered teardown will occur
+      </li>
+      <li>
+        If you complete, error, or unsubscribe, you are guaranteed to clean up resources
+      </li>
+    </ul>
+    `,
+  },
+];
+
+export const OBSERVER_SECTION: SECTION[] = [
+  {
+    title: '',
+    body: `
+    <p>
+      An Observer is a consumer of values delivered by an Observable. Observers are simply a set of callbacks, one for each type of notification delivered by the Observable: next, error, and complete. 
+    </p>
+    <p>
+      The following is an example of a typical Observer object:
+    </p>
+    <code>
+    <pre>
+    const observer = {
+      next: x => console.log('Observer got a next value: ' + x),
+      error: err => console.error('Observer got an error: ' + err),
+      complete: () => console.log('Observer got a complete notification'),
+    };
+    </pre>
+    </code>
+    <p>
+      <b>Observers are just objects with three callbacks</b>, one for each type of notification that an Observable may deliver.
+    </p>
+    `,
+  },
+  {
+    title: 'How do I use an Observer?',
+    body: `
+    <p>
+      To use the Observer, provide it to the subscribe of an Observable:
+    </p>
+    <code>
+    <pre>
+    observable.subscribe(observer);
+    </pre>
+    </code>
+    `,
+  },
+  {
+    title: 'Observers are partial',
+    body: `
+    <p>
+    If you don't provide one of the callbacks, the execution of the Observable will still happen normally, except some types of notifications will be ignored, because they don't have a corresponding callback in the Observer.
+    </p>
+    <code>
+    <pre>
+    const observer = {
+      next: x => console.log('Observer got a next value: ' + x),
+      error: err => console.error('Observer got an error: ' + err),
+    };
+    </pre>
+    </code>
     `,
   },
 ];
